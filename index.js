d922ce285be0673164c29f8bc4db1fbfce32ae6b
João Victor <90943414+Milk21291@users.noreply.github.com>
2023-03-03 15:15:31 -0300

    Update index.js

--- index.js
+++ index.js
@@ -12,69 +12,62 @@ const hour = date.getHours();
 const minuts = date.getMinutes();
 
 // Day of the week
-if(dayOfTheWeek  === 0) {
-    dayOfWeekText = 'Domingo';
-    } else if(dayOfTheWeek === 1) {
-        dayOfWeekText = 'Segunda-feira';
-    } else if(dayOfTheWeek === 2) {
-        dayOfWeekText = 'Terça-feira';
-    } else if(dayOfTheWeek === 3) {
-        dayOfWeekText = 'Quarta-feira';
-    } else if(dayOfTheWeek === 4) {
-        dayOfWeekText = 'Quinta-feira';
-    } else if(dayOfTheWeek === 5) {
-        dayOfWeekText = 'Sexta-feira';
-    } else if(dayOfTheWeek === 6) {
-        dayOfWeekText = 'Sábado';
+    const daysWeek = {
+        0: 'Domingo',
+        1: 'Segunda-feira',
+        2: 'Terça-feira',
+        3: 'Quarta-feira',
+        4: 'Quinta-feira',
+        5: 'Sexta-feira',
+        6: 'Sábado',
     };
 
+    function weeks(dayWeek) {
+        return daysWeek[dayWeek]
+    };
+
+    dayOfWeekText = weeks(dayOfTheWeek);
+
     // Day
-    if(day  === 1) {
-        dayText = '01';
-        } else if(day === 2) {
-            dayText = '02';
-        } else if(day === 3) {
-            dayText = '03';
-        } else if(day === 4) {
-            dayText = '04';
-        } else if(day === 5) {
-            dayText = '05';
-        } else if(day === 6) {
-            dayText = '06';
-        } else if(day === 7) {
-            dayText = '07';
-        } else if(day === 8) {
-            dayText = '08';
-        } else if(day === 9) {
-            dayText = '09';
+        const days = {
+            1: '01',
+            2: '02',
+            3: '03',
+            4: '04',
+            5: '05',
+            6: '06',
+            7: '07',
+            8: '08',
+            9: '09',
         };
 
-    // Mouth
-    if(month === 0) {
-        monthText = 'Janeiro';
-        } else if(month === 1) {
-            monthText = 'Fevereiro';
-        } else if(month === 2) {
-            monthText = 'Março';
-        } else if(month === 3) {
-            monthText = 'Abril';
-        } else if(month === 4) {
-            monthText = 'Maio';
-        } else if(month === 5) {
-            monthText = 'Junho';
-        } else if(month === 6) {
-            monthText = 'Julho';
-        } else if(month === 7) {
-            monthText = 'Agosto';
-        } else if(month === 8) {
-            monthText = 'Setembro';
-        } else if(month === 9) {
-            monthText = 'Outubro';
-        } else if(month === 10) {
-            monthText = 'Novembro';
-        } else if(month === 11) {
-            monthText = 'Dezembro';
+        function daysWithZero(withZero) {
+            return days[withZero]
         };
+
+        dayText = daysWithZero(day);
+
+    // Mouth
+        const dayMonths = {
+            0: 'Janeiro',
+            1: 'Fevereiro',
+            2: 'Março',
+            3: 'Abril',
+            4: 'Maio',
+            5: 'Junho',
+            6: 'Julho',
+            7: 'Agosto',
+            8: 'Setembro',
+            9: 'Outubro',
+            10: 'Novembro',
+            11: 'Dezembro',
+        };
+
+        function months(month) {
+            return dayMonths[month]
+        };
+
+        monthText = months(month);
     
     resultado.innerHTML = `<h1> ${dayOfWeekText}, ${dayText} de ${monthText} de ${year} às ${hour}:${minuts} </h1>`;
     
